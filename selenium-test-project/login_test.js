6022c7bf514a8d39a18190087ac6be6c8b1475c2
mo-Oka <mo3aika@bk.ru>
2017-10-26 09:39:11 -0400

    added 'catch-finally' construction for admin login

--- selenium-test-project/login_test.js
+++ selenium-test-project/login_test.js
@@ -5,25 +5,35 @@ test.describe('Admin Login', function() {
     let driver;
 
     test.before(function *() {
-        driver = yield new Builder().forBrowser('firefox').build();
+        driver = yield new Builder().forBrowser('chrome').build();
     });
 
-    it('works with promises', function() {
+    try {
+
+    it('works with promises', function () {
         return driver.get('http://localhost:8080/litecart/admin/')
             .then(_ =>
                 driver.findElement(By.name('username')).sendKeys('admin'))
             // .then(_ =>
             //     driver.findElement(By.name('password')).sendKeys('admin', Key.RETURN)) //test implemented with enter button
-             .then(_ =>
-                 driver.findElement(By.name('password')).sendKeys('admin')) //test implemented with a click on the UI button
-             .then(_ =>
-                 driver.findElement(By.name('login')).click())
+            .then(_ =>
+                driver.findElement(By.name('password')).sendKeys('admin')) //test implemented with a click on the UI button
+            .then(_ =>
+                driver.findElement(By.name('login')).click())
             // .then(_ =>
             //     driver.wait(until.elementIsVisible(notice), 1000))
             .then(_ =>
                 driver.wait(until.elementLocated(By.xpath('//*[@id="notices"]/div[2]/i')), 1000)); // assert by notice "You are now logged in as admin"
-      });
+    });
 
     test.after(() => driver.quit());
+    }
+
+    finally {
+        driver.quit();
+    }
 });
+
+
+
 
