f6002607dc3bc092a278c50777da82dd924d2e6f
mo-Oka <mo3aika@bk.ru>
2017-11-06 20:41:22 -0500

    collect all admin page tests in one suite

--- selenium-test-project/admin_menu_test.js
+++ selenium-test-project/admin_menu_test.js
@@ -1,20 +1,32 @@
-const {Builder, By, Key} = require('selenium-webdriver');
+const {Builder, By, Key, logging, until} = require('selenium-webdriver');
 const test = require('selenium-webdriver/testing');
+// logging.installConsoleHandler();
+// logging.getLogger('promise.ControlFlow').setLevel(logging.Level.ALL);
 
     test.describe('Admin Menu', function () {
         let driver;
-        this.timeout(10000);
+        this.timeout(30000);
 
         test.before(function *() {
             driver = yield new Builder().forBrowser('chrome').build();
         });
 
+            test.it('login by admin - promises', function () {
+                return driver.get('http://localhost:8080/litecart/admin/')
+                    .then(_ =>
+                        driver.findElement(By.name('username')).sendKeys('admin'))
+                    // .then(_ =>
+                    //     driver.findElement(By.name('password')).sendKeys('admin', Key.RETURN)) //test implemented with enter button
+                    .then(_ =>
+                        driver.findElement(By.name('password')).sendKeys('admin')) //test implemented with a click on the UI button
+                    .then(_ =>
+                        driver.findElement(By.name('login')).click())
+                    .then(_ =>
+                        driver.wait(until.elementLocated(By.xpath("//*[contains(., 'You are now logged in as admin')]")), 1000)); // assert by notice "You are now logged in as admin"
+            });
+
         try {
             test.it('checks if there h1 in every menu option', function* () {
-                //login by admin
-                yield driver.get('http://localhost:8080/litecart/admin');
-                yield driver.findElement(By.name('username')).sendKeys('admin');
-                yield driver.findElement(By.name('password')).sendKeys('admin', Key.RETURN);
                 //find menu elements as array
                 var links = yield driver.findElements(By.css('#box-apps-menu > li > a')).then();
                 for (var i = 0; i < links.length; i++) {
@@ -33,7 +45,7 @@ const test = require('selenium-webdriver/testing');
 
                             });
                             //assert by finding h1 on the page
-                            yield driver.findElement(By.css('h1'));
+                            driver.wait(until.elementLocated(By.css('h1')));
                     }
                 }
             });
